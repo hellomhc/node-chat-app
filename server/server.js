ed9036b846a1633487319d6e3bc4ac68127c982f
Myeonghwan Cho <MyeonghCho@gmail.com>
2018-07-27 20:22:35 +0900

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ var io = socketIO(server);
 io.on('connection', socket => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Aaron',
+    text: 'Hey!',
+    createAt: 123456
+  });
+
+  socket.on('createMessage', message => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
